1f46f16de23cfba08f40d49095df3ad2d4c4dae4
Matt Millican <matt@mattmillican.com>
2021-06-14 23:46:44 -0500

    Add membership List method and authorize plans controller

--- src/MemberPro.Api/Controllers/MembershipPlansController.cs
+++ src/MemberPro.Api/Controllers/MembershipPlansController.cs
@@ -1,12 +1,15 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using MemberPro.Core.Models.Plans;
 using MemberPro.Core.Services.Plans;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MemberPro.Api.Controllers
 {
     [ApiController]
     [Route("plans")]
+    [Authorize]
     public class MembershipPlansController : ControllerBase
     {
         private readonly IMembershipPlanService _membershipPlanService;
@@ -26,6 +29,14 @@ namespace MemberPro.Api.Controllers
             }
 
             return Ok(plan);
+        }
+
+        [HttpGet("")]
+        public async Task<ActionResult<List<MembershipPlanModel>>> List()
+        {
+            var plans = await _membershipPlanService.GetAllAsync();
+
+            return Ok(plans);
         }
     }
 }
