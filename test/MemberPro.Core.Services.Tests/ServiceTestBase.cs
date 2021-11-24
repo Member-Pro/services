d18a777d34eb18a45c7a98bbf461e30dcb8ae8ed
Matt Millican <matt@mattmillican.com>
2021-11-23 21:17:50 -0600

    Change tests to use SQLite in memory db

--- test/MemberPro.Core.Services.Tests/ServiceTestBase.cs
+++ test/MemberPro.Core.Services.Tests/ServiceTestBase.cs
@@ -1,21 +1,30 @@
+using System;
+using System.Data.Common;
 using AutoMapper;
 using MemberPro.Core.Data;
 using MemberPro.Core.Data.Implementations;
 using MemberPro.Core.Models;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace MemberPro.Core.Services.Tests
 {
-    public abstract class ServiceTestBase
+    public abstract class ServiceTestBase : IDisposable
     {
+        private readonly DbConnection _dbConnection;
+
         protected IDbContext DbContext { get; }
         protected IMapper Mapper { get; }
 
         protected ServiceTestBase()
         {
             var dbContextOptions = new DbContextOptionsBuilder<MemberProDbContext>()
-                .UseSqlite("Filename=test.db")
+                .UseSqlite(CreateInMemoryDatabase())
+                // .UseInMemoryDatabase(databaseName: "MemberProTests")
                 .Options;
+
+            _dbConnection = RelationalOptionsExtension.Extract(dbContextOptions).Connection;
 
             DbContext = new MemberProDbContext(dbContextOptions);
 
@@ -28,6 +37,15 @@ namespace MemberPro.Core.Services.Tests
 
             InitDatabase();
         }
+
+        private static DbConnection CreateInMemoryDatabase()
+        {
+            var connection = new SqliteConnection("Filename=:memory:");
+            connection.Open();
+            return connection;
+        }
+
+        public void Dispose() => _dbConnection.Dispose();
 
         protected virtual void InitDatabase()
         {
